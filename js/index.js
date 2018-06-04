ee74f2604cc89c110717f7d905d1587f4a2bd66c
ZiroGX2 <zirogx2@gmail.com>
2018-06-04 08:57:19 -0400

    ayyayeyay

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 170,
+      "value": 250,
       "density": {
         "enable": true,
         "value_area": 1000.1416867389551
